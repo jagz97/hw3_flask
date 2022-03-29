062dab7b731fcde1ddf8a38440b9213cbdd41dd9
Jagjit Singh <jagjitsingh@Jagjits-MacBook-Pro.local>
2022-03-28 19:08:36 -0700

    first commit

---
+++ app/home.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template_string
+name = 'Lisa'
+city_names = ['Paris', 'London', 'Rome', 'Tahiti']
+
+
+myobj_app = Flask(__name__)
+
+
+@myobj_app.route('/')
+def home():
+
+    return render_template_string("""
+    <html>
+           <h1>
+                Welcome {{name}}!
+            </h1>
+                <body>
+                <a href='www.google.com'>not google</a>
+                <ul>
+                {% for i in city_name  %}
+                <li>{{i}}</li>
+                {% endfor %}
+                </ul>
+                
+                </body>
+                
+                
+
+    </html >""", city_name=city_names, name=name)
+
+
+myobj_app.run()
